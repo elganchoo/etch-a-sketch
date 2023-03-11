101eac6f924767dce802af4fa737de1c100a3e88
elganchoo <srgandimitrovski8@gmail.com>
2023-03-11 14:04:17 +0100

    The mechanics of the app work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector('.container');
 const contWidth=container.offsetWidth;
-let grab = false;
+let grab = true;
 let n = 16;
-function f(){
+
+function setGrid(){
+    let input = window.prompt("Enter the number of rows and columns: ");
+    while(input>100) input = window.prompt("Please enter a value smaller than 100: ");
+    container.replaceChildren();
+    f(Number(input));
+}
+
+function f(n=16){
 for(let i=0; i<(n*n); i++){
     const div = document.createElement('div');
     div.setAttribute('class', 'grid');
@@ -15,9 +23,9 @@ for(let i=0; i<(n*n); i++){
             if(grab) {
                 e.target.style.backgroundColor = "black";
             }
-            window.addEventListener('mouseup', function () {grab=false;})
-        })
 
+        })
+        container.addEventListener('click', function () {grab=false;})
     })
 
 }
